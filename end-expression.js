ce9c91bb6599983496a658763a35d153116e895d
wenyi <wenyi@wenyideMacBook-Pro.local>
2018-04-22 16:18:10 +0800

    后缀表达式

--- end-expression.js
+++ end-expression.js
@@ -1,6 +1,14 @@
+let numberReg = /\d/;
+let plusReg = /\+/;
+let minusReg = /-/;
+let timesReg = /\*/;
+let dividReg = /\//;
+let leftReg = /\(/;
+let rightReg = /\)/;
+let operatorValue = { '+': 0, "-": 0, "*": 1, "/": 1 };
+
 function calculate(expression = "") {
     let stack = [];
-    let numberReg = /\d/;
     let expressionLength = expression.length;
     for (let i = 0; i <= expressionLength - 1; i++) {
         let item = expression[i];
@@ -9,7 +17,7 @@ function calculate(expression = "") {
         } else {
             let n1 = parseInt(stack.pop());
             let n2 = parseInt(stack.pop());
-            let midResult = baseCalculate(n2, n1 , item);
+            let midResult = baseCalculate(n2, n1, item);
             stack.push(midResult);
             console.log({ n1, n2, item, midResult, stack })
         }
@@ -27,4 +35,42 @@ function baseCalculate(n1, n2, operator) {
     }
 }
 
+function translate(expression = "") {
+    let operatorStack = [];
+    let midStack = [];
+    for (let c of expression) {
+        console.log(c)
+        if (c.match(numberReg)) {
+            midStack.push(c);
+        } else if (c.match(plusReg) || c.match(minusReg) || c.match(timesReg) || c.match(dividReg)) {
+            if (operatorStack.length == 0 || operatorStack[operatorStack.length - 1].match(leftReg)) {
+                operatorStack.push(c);
+            } else {
+                while (!compare(c, operatorStack[operatorStack.length - 1]) && operatorStack.length > 0) {
+                    midStack.push(operatorStack.pop());
+                }
+                operatorStack.push(c)
+            }
+        } else if (c.match(leftReg)) {
+            operatorStack.push(c);
+        } else if (c.match(rightReg)) {
+            while (!operatorStack[operatorStack.length - 1].match(leftReg) && operatorStack.length > 0) {
+                midStack.push(operatorStack.pop());
+            }
+            operatorStack.pop();
+        }
+    }
+    while (operatorStack.length > 0) {
+        midStack.push(operatorStack.pop());
+    }
+    let endExp = midStack.join('');
+    console.log(endExp);
+    return endExp;
+}
+
+function compare(newOperator, topOperator) {
+    return operatorValue[topOperator] < operatorValue[newOperator];
+}
+
+translate('1+3*4*(2+7)-6*5')
 calculate('134*27+*+65*-')
